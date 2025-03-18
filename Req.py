f7ca36c13d86c7c5817a61967eef84c2248b8a5c
Rune Johannesen <rujo@netdesign.dk>
2025-03-18 10:09:30 +0100

    Better typing

--- Req.py
+++ Req.py
@@ -1,7 +1,7 @@
 # Developed by Rune Johannesen @2021-2023
 from aiohttp import ClientSession, BasicAuth, TCPConnector
 from asyncio import gather, sleep, create_task, Semaphore
-from typing import Union
+from typing import Union, Any
 from json import dumps
 from os.path import splitext, basename
 from General_logger import setup_logger
@@ -20,28 +20,29 @@ class Req():
         self.__HEADERS: dict = headers if headers else {'Content-Type': 'application/json', 'Accept': 'application/json', 'cache-control': 'no-cache'}
         self.__TIMEOUT: int = timeout if timeout else 60
         self.__RATE_LIMIT: int = rate_limit
-        self.__USE_SSL: bool = TCPConnector(verify_ssl=True) if use_ssl else TCPConnector(verify_ssl=False)
+        self.__USE_SSL = TCPConnector(verify_ssl=True) if use_ssl else TCPConnector(verify_ssl=False)
         self.__AUTH: str = None
         self.__LOGGER = setup_logger(splitext(basename(__file__))[0])
         if auth:
             try:
                 usernm,passwd = auth.split(" , ")
-                self.__AUTH: str = BasicAuth(usernm,passwd)
+                self.__AUTH = BasicAuth(usernm,passwd)
             except: raise Exception("There was a problem with auth. You must separate the username and password with \" , \" (space comma space)")
 
-    def returnPartionedList(self, inputlist: list) -> list:
+    def returnPartionedList(self, inputlist: list) -> list[list[Any]]:
         """Returns a list split into segments of self.RATE_LIMIT
+
         :inputlist: list (Required) List of lists containing data to split into segments. Example: [[data],[data],[data],etc...]"""
         return([inputlist[i:i + self.__RATE_LIMIT] for i in range(0, len(inputlist), self.__RATE_LIMIT)])
 
-    async def __req(self, url: str, session: ClientSession, method: str, payload: str, semaphore: Semaphore, validate: bool = False) -> Union[dict,str]:
-        """Private method\n
-        Returns the request as either a dict or string. Dictionary will always be the preferred return type
+    async def __req(self, url: str, session: ClientSession, method: str, payload: str, semaphore: Semaphore, validate: bool = False) -> Union[dict[Any,Any],str]:
+        """Private method. Returns the request as either a dict or string. Dictionary will always be the preferred return type
+
         :url: string (Required) Url to request or post data to/from
         :method: string (Required) Supported request methods: get, post, put, delete and patch
         :payload: string (Required) Request payload. Leave blank ('') for no payload
-        :validate: boolean (Optional) Method to retry a request that fails. Default is False\n
-        If response status is (500) Internal Server Error, the request will be retried after 10 seconds"""
+        :validate: boolean (Optional) Method to retry a request that fails. Default is False
+        :NOTE: If response status is (500) Internal Server Error, the request will be retried after 10 seconds"""
         try:
             async with semaphore:
                 async with session.request(method=method, url=url, data=payload, timeout=self.__TIMEOUT) as response:
@@ -49,9 +50,9 @@ class Req():
                         if response.status == 202:
                             try: return(str(response.headers['location'].split("submit/",1)[1]))
                             except: pass
-                        try: r: dict = await response.json()
+                        try: r: dict[Any,Any] = await response.json()
                         except: r: str = await response.text()
-                        if not r: r: dict = {"OK":f"{method}","HEADERS":response.headers}
+                        if not r: r: dict[str,Any] = {"OK":f"{method}","HEADERS":response.headers}
                         return(r)
                     elif response.status == 500:
                         if not validate:
@@ -60,7 +61,7 @@ class Req():
                         if payload: self.__LOGGER.info(f"Server Error: {response.status} -> Operation: {method} -> URL: {url} -> Payload:\n{payload}")
                         else: self.__LOGGER.info(f"Server Error: {response.status} -> Operation: {method} -> URL: {url}")
                     else:
-                        try: r: dict = await response.json()
+                        try: r: dict[Any,Any] = await response.json()
                         except: r: str = await response.text()
                         if not r: r: str = "N/A"
                         if payload: self.__LOGGER.info(f"Client Error: {self.__HTTP_ERR_MAP[response.status]} -> Operation: {method} -> URL: {url}\nPayload:\n{payload}\nResponse:\n{r}\n")
@@ -70,16 +71,16 @@ class Req():
             else: self.__LOGGER.info(f"Exception Error -> Operation: {method} -> URL: {url}\n{format_exc()}")
         return({})
 
-    async def make_requests(self, req_list: list) -> list:
+    async def make_requests(self, req_list: list[list[str]]) -> list[list[Union[dict[Any,Any],str]]]:
         """Returns the results from the requests in req_list in a list of lists format, example: [[response],[response],etc...]
+
         :req_list: list (Required) List of lists with requests to be processed
-            Structure: [[method: str (Required), url: str (Required), payload: dict (Optional)],[...]]
-            Allowed methods: get, post, put, delete and patch
-            Payload must be a dictionary, it will be converted to a string
-        req_list Examples:
-            [
-                ["GET","www.example.com"],\n
-                ["POST","www.example.com",{"some":"payload"}],\n
+        - :Structure: [[method: str (Required), url: str (Required), payload: dict (Optional)],[...]]
+        - :Allowed methods: get, post, put, delete and patch. Payload must be a dictionary, it will be converted to a string
+        
+        :req_list Examples: [
+                ["GET","www.example.com"],
+                ["POST","www.example.com",{"some":"payload"}],
                 ["PUT","www.example.com",{"some":"payload"}]
             ]"""
         def check_payload(entry: list) -> Union[str,None]:
@@ -91,8 +92,8 @@ class Req():
         if not req_list or not req_list[0]:
             raise Exception("req_list cannot be empty, you must provide requests to be processed.\n\nThe format is (list of lists):\n    [[method: str (Required), url: str (Required), payload: dict (Optional)],[...]]")
         resultsList: list = []
-        partitions: list = self.returnPartionedList(req_list)
-        semaphore: Semaphore = Semaphore(10)
+        partitions = self.returnPartionedList(req_list)
+        semaphore = Semaphore(10)
         async with ClientSession(auth=self.__AUTH, headers=self.__HEADERS, connector=self.__USE_SSL, timeout=self.__TIMEOUT*2) as session:
             for partition in partitions:
                 partitionTasks: list = []
@@ -100,10 +101,10 @@ class Req():
                     if "get" in entry[0].lower():
                         partitionTasks.append(create_task(self.__req(entry[1],session,entry[0],check_payload(entry),semaphore)))
                     else:
-                        result: Union[str,dict] = await self.__req(entry[1],session,entry[0],check_payload(entry),semaphore)
+                        result = await self.__req(entry[1],session,entry[0],check_payload(entry),semaphore)
                         resultsList.append(result)
                 if partitionTasks:
-                    results: list = await gather(*partitionTasks)
+                    results: list[list[Union[dict[Any,Any],str]]] = await gather(*partitionTasks)
                     for result in results: resultsList.append(result)
                 await sleep(1.1)
         return(resultsList)
